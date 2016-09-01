0a4faf5104dd60b17957e73b1f082e65af68c6b2
Ta Thanh Dinh <tathanhdinh@gmail.com>
2016-09-01 12:05:46 +0200

    add others

---
+++ rootMe/readelf/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace readelf
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var shouldParseSections = false;
+			var shouldParseSegments = false;
+			var fileName = "";
+
+			var options = new Mono.Options.OptionSet {
+				{ "f|file", "input ELF file", fileOpt => fileName = fileOpt },
+				{ "segs|segments", "parse segments", segOpt => shouldParseSegments = (segOpt != null) },
+				{ "secs|sections", "parse sections", secOpt => shouldParseSections = (secOpt != null) }
+			};
+
+			// Console.WriteLine ("Hello World!");
+			var fileList = options.Parse(args);
+
+			if (fileList.Count == 0) {
+				System.Console.WriteLine("please give an input ELF file");
+			}
+			else {
+				fileName = fileList[0];
+				var elfReader = ELFSharp.ELF.ELFReader.Load(fileName);
+				
+				// parse segments
+				if (shouldParseSegments) {
+          var segments = elfReader.Segments;
+					var segmentNumber = 0;
+					foreach (var seg in segments) {
+						System.Console.WriteLine("segment {0}", seg.Type.ToString());
+						segmentNumber++;
+					}
+					System.Console.WriteLine("{0} segment parsed.", segmentNumber);
+				}
+
+				// parse sections
+				if (shouldParseSections) {
+					var sections = elfReader.Sections;
+					var sectionNumber = 0;
+					foreach (var sec in sections) {
+						System.Console.WriteLine("section {0}", sec.Name);
+						sectionNumber++;
+					}
+					System.Console.WriteLine("{0} section parsed.", sectionNumber);
+				}
+			}
+		}
+	}
+}
---
+++ rootMe/rv0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace rv0
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			if (args.Length == 0) {
+				System.Console.WriteLine("Please give an input argument. (e.g. ./rv0 input_argument)");
+			}
+			else {
+				var accValue = 0xdefec8edU;
+				var inputArg = args[0];
+				foreach (var c in inputArg) {
+					var cUint = Convert.ToUInt32(c);
+					accValue = ((accValue + cUint) ^ (accValue - cUint)) + accValue;
+					// System.Console.WriteLine("accValue = 0x{0:x} (before rotate)", accValue);
+					accValue = (accValue << 8) | (accValue >> 24);					
+				}
+				System.Console.WriteLine("accValue = 0x{0:x}", accValue);
+				System.Console.WriteLine("{0:s}", (accValue == 0xa2edc5c4U) ? "Bravo, you got the key!!!" : "Try again, please :-)"); 
+			}
+		}
+	}
+}
